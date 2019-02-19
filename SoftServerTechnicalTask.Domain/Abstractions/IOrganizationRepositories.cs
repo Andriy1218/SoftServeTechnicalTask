dc43051b4a1e8b8e81615c72735348898edf6b23
Andriy1218 <andriy1218@gmail.com>
2019-02-19 10:48:42 +0200

    Added repository abstractuion to domain

---
+++ SoftServerTechnicalTask.Domain/Abstractions/IOrganizationRepositories.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftServerTechnicalTask.Domain.Abstractions
+{
+    public interface IGenericRepositories<T>
+    {
+        T Get(int organizationId);
+        void Create(T organization);
+        void Update(T organization);
+        void Delete(int organizationId);
+    }
+}
